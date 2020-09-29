9c68469f4b6a826f7a813f50e885e5500c912c67
Laura Lujan <lauralujanhdez@gmail.com>
2020-09-29 16:06:55 -0500

    implemented function to color the blocks on mouse over

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-let container = document.querySelector('.container');
+let container = document.querySelector(".container");
 
 function makeGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
-    for (let cells = 0; cells < (rows * cols); cells++) {
-      let cell = document.createElement("div");
-      container.appendChild(cell).className = "cell";
-    };
-  };
-  
-  makeGrid(16, 16);
+  container.style.setProperty("--grid-rows", rows);
+  container.style.setProperty("--grid-cols", cols);
+  for (let cells = 0; cells < rows * cols; cells++) {
+    let cell = document.createElement("div");
+    container.appendChild(cell).className = "cell";
+    let elements = document.querySelectorAll('.cell');
+    elements.forEach(function(element){
+      element.addEventListener('mouseover', function(event){
+        event.target.classList.add('draw')
+      })
+    } )
+
+  }
+}
+
+makeGrid(16, 16);
